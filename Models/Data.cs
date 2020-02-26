8ccac2e431578a07caa3cf0ac504ecf8f329f0e1
Stasiak, Daniel <dan@stasiak.net>
2020-02-26 13:25:36 +0100

    Delete Data.cs

--- Models/Data.cs
+++
@@ -1,9 +0,0 @@
-namespace NetfieldDeviceSample.Models
-{
-    public class Data
-    {
-        public string DataType {get; set;} // shall be "actual" or "target"
-        public int Temperature {get; set;}
-        public int Speed {get; set;}
-    }
-}
