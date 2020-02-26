2156a9b8075a906ac0b99822dd653deda0f0fc5e
Stasiak, Daniel <dan@stasiak.net>
2020-02-26 13:24:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NetfieldDeviceSample.Models;
+
+namespace NetfieldDeviceSample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                          .ConfigureAppConfiguration((hostingContext, config) =>
+              {
+                  var env = hostingContext.HostingEnvironment;
+                  config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                  config.AddEnvironmentVariables();
+
+                  if (args != null)
+                  {
+                      config.AddCommandLine(args);
+                  }
+              })
+
+              .ConfigureServices((hostContext, services) =>
+              {
+                  services.AddOptions();
+                  services.Configure<MqttConfig>(hostContext.Configuration.GetSection("MqttConfig"));
+                  services.AddTransient<IHostedService, Application>();
+              })
+              .ConfigureLogging((hostingContext, logging) =>
+              {
+                  logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                  logging.AddConsole();
+              })
+              .Build();
+            builder.Run();
+        }
+    }
+}
