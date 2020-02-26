506d2de722007ffb4fbec34c8d6e50e1cfd4d682
Stasiak, Daniel <dan@stasiak.net>
2020-02-26 13:26:03 +0100

    Delete MqttPubData.cs

--- Models/MqttPubData.cs
+++
@@ -1,9 +0,0 @@
-namespace NetfieldDeviceSample.Models
-{
-    public class MqttPubData
-    {
-        public string DataType {get; set;} // shall be "actual" or "target"
-        public int Temperature {get; set;}
-        public int Speed {get; set;}
-    }
-}
